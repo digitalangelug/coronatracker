635e4f23cabac8b9a57429182fe3bf7a6befd3a3
Brian Wakabi <b.wakabi@corp.harissint.com>
2020-06-07 23:12:02 +0300

    Updates

---
+++ precache-manifest.345cebcbaa4d22a18898b7d4c39b25bb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0eeadb8c63c5a3965071c36ec25abc41",
+    "url": "/coronatracker/index.html"
+  },
+  {
+    "revision": "28402a93fd363789c85c",
+    "url": "/coronatracker/static/css/main.c78f55b5.chunk.css"
+  },
+  {
+    "revision": "7822685c5cedb6e4749b",
+    "url": "/coronatracker/static/js/2.0c6a2581.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/coronatracker/static/js/2.0c6a2581.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28402a93fd363789c85c",
+    "url": "/coronatracker/static/js/main.e5218c28.chunk.js"
+  },
+  {
+    "revision": "c742909ebbfd101033b8",
+    "url": "/coronatracker/static/js/runtime-main.861764c1.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/coronatracker/static/media/image.d7265326.png"
+  }
+]);
